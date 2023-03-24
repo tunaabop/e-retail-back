fd54d66c0eab5c4d6752675072eaf45af413bed4
tunaabop <tinawang158@gmail.com>
2023-03-24 01:30:14 -0400

    added post and delete routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,10 +3,9 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// GET route to find all categories and include its associated Products
 router.get('/', (req, res) => {
-  // find all categories and include its associated Products
   Category.findAll({
-    // find all categories
     attributes: ["id", "category_name"],
     include: 
     [{
@@ -20,9 +19,8 @@ router.get('/', (req, res) => {
     });
 });
 
+// GET route to find one category by its `id` value, including its associated Products
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   Category.findOne({
     // find category by requested id
     where: {id: req.params.id},
@@ -44,16 +42,47 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// POST route to create a new category
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({category_name: req.body.category_name,})
+    .then((categoryData) => res.json(categoryData))
+    .catch((err) =>{
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
+// PUT route to update a category by its `id` value
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(
+    {category_name: req.body.category_name},
+    {where: { id: req.params.id }}
+  )
+  .then((categoryData) => {
+    if (!categoryData) {
+      res.status(404).json({ message: "Cannot find category" }); return;
+    }
+    res.json(categoryData);
+  });
 });
 
+  // Delete route, or a category by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({where: {id: req.params.id}})
+    .then((categoryData) => {
+      if (!categoryData) {
+        res.status(404).json({ message: "Cannot find category" });
+        return;
+      }
+      
+      res.json(categoryData);
+
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// Export routes
 module.exports = router;
