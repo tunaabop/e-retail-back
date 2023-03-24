cf384f93d9ba278a383e1c74d3cf7abe103c2627
tunaabop <tinawang158@gmail.com>
2023-03-24 01:35:42 -0400

    added get routes for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,54 @@
 const router = require('express').Router();
+// importing models here
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
+// GET route to get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ["id", "product_name", "price", "stock", "category_id"],
+    // making sure to include its associated Category and Tag data
+    include: 
+    [{
+        model: Category,
+        attributes: ["category_name"],
+    }]
+  }).then((productData) => res.json(productData))
+    .catch((err) => {res.status(500).json(err);});
 });
 
-// get one product
+// GET route to get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: 
+    {
+      id: req.params.id
+    },
+    attributes: ["id", "product_name", "price", "stock", "category_id"],
+    // be sure to include its associated Category and Tag data
+    include: 
+    [
+      {
+        model: Category,
+        attributes: ["category_name"],
+      }
+    ]
+  }).then((productData) => {
+      if (!productData) {
+        res.status(404).json({ message: "No product found" });
+        return;
+      }
+      res.json(productData);
+    })
+    .catch((err) => 
+    {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  
 });
 
 // create new product
